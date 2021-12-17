ba0bcbc7ec35b8c7943aaae51760fba9cf9aff7a
Darsh Sikka <67545017+DarshSikka@users.noreply.github.com>
2021-12-17 11:49:10 +0530

    spin gets a huge nerf

--- Game.js
+++ Game.js
@@ -204,7 +204,24 @@ class Game {
         }
       }
     } else if (this.ballSelection == weakness) {
-      if (this.innings && this.scores[1] > this.scores[0]) {
+      const rand = Math.floor(Math.random() * 10 + 1);
+      if (this.spin && rand <= 5) {
+        this.scores[this.innings ? 1 : 0] += 1;
+        this.channel.send(
+          `Wide ball there, 1 more run to the batsman, stands at ${
+            this.scores[this.innings ? 1 : 0]
+          }`
+        );
+        if (this.innings == true) {
+          if (this.scores[1] > this.scores[0]) {
+            console.log("should be over");
+            this.over = true;
+            this.updateLeaderboard(true);
+            return this.channel.send(`<@${this.bat.id}> wins, chased down`);
+          }
+          this.channel.send(`${this.scores[0] + 1 - this.scores[1]} to win`);
+        }
+      } else if (this.innings && this.scores[1] > this.scores[0]) {
         this.channel.send(`<@!${this.bat.id}> wins`);
         this.over = true;
         this.updateLeaderboard(true);
