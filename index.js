b148ab8be89637f0599ba5c95d27b9a32fea20cd
Darsh Sikka <67545017+DarshSikka@users.noreply.github.com>
2021-12-23 20:09:14 +0530

    add non-expiring server invite

--- index.js
+++ index.js
@@ -263,7 +263,7 @@ client.on("messageCreate", async (message) => {
       .setURL(link);
     message.channel.send({ embeds: [embed] });
   } else if (command == "cg!server") {
-    const link = "https://discord.gg/aujd5rXG";
+    const link = "https://discord.gg/TgAQte9zgE";
     const embed = new MessageEmbed().setTitle("join cg server").setURL(link);
     message.channel.send({ embeds: [embed] });
   }
